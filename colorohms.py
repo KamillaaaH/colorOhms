f89f47a082b31320e0c22f85fc9b51b5f2a222e6
Amanda Tresbach <atresbach@gmail.com>
2012-11-10 18:47:24 -0200

    adicionando alguns campos primeira caixa de seleção (combobox)

--- colorohms.py
+++ colorohms.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 import sys
 try:
@@ -14,6 +15,20 @@ except:
 
 class colorohmsGUI:
 
+    def init_multiple(self):
+        self.liststore = gtk.ListStore(int,str)
+        self.liststore.append([0,"selecione"])
+        self.liststore.append([1,"Ω"])
+        self.liststore.append([2,"kΩ"])
+        self.liststore.append([3,"MΩ"])
+        
+        self.combobox = self.builder.get_object('multiple')
+        self.combobox.set_model(self.liststore)
+        self.cell = gtk.CellRendererText()
+        self.combobox.pack_start(self.cell, True)
+        self.combobox.add_attribute(self.cell, 'text', 1)
+        self.combobox.set_active(0)
+
     def __init__(self):
         self.builder = gtk.Builder()
         self.builder.add_from_file("colorohms.glade")
@@ -21,6 +36,7 @@ class colorohmsGUI:
         if self.window:
             self.window.connect("destroy", gtk.main_quit)        
         self.builder.connect_signals(self)
+        self.init_multiple()
 
 app = colorohmsGUI()
 gtk.main()
